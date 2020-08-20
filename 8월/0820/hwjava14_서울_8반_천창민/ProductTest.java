6cb9346fc6b644629cb53da59d7671d860654bb7
USER <USER@DESKTOP-QC7J3E5>
2020-08-20 22:56:35 +0900

    과제 제출

--- 8월/0820/hwjava14_서울_8반_천창민/ProductTest.java
+++ 8월/0820/hwjava14_서울_8반_천창민/ProductTest.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 
-import com.ssafy.Book;
-import com.ssafy.BookDAO;
+import hwjava14_서울_8반_천창민.Product;
+import hwjava14_서울_8반_천창민.ProductDAO;
 
 public class ProductTest {
 	public static void main(String[] args) throws NumberFormatException, IOException {
